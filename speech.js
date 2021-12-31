63017fa6eba2e3b6550bd0d5ea2bc8aad8bf9f1e
John Doe <johndoe@example.com>
2021-12-31 12:54:24 +0530

    stylings added

--- speech.js
+++ speech.js
@@ -4,7 +4,6 @@ const result=document.querySelector(".result");
 const speechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition=new speechRecognition();
 recognition.continuous = true;
-const greetings=["Iam happy to hear your voice","I love your voice"]
 
 recognition.onstart=()=>{
 	console.log("mic is turned on start speaking")
@@ -41,21 +40,15 @@ function readOutLoud(msg){
 	const speech=new SpeechSynthesisUtterance();
 	speech.text="Sorry I can't understand you"
 	if(msg.includes("how are you")){
-		//const finalResult=greetings[Math.floor(Math.random()*greetings.length)];
-		//speech.text=finalResult;
 		speech.text="It is really a plesant day for me! How may I help you?"
 	}
 	if(msg.includes("time")){
 		const today=new Date();
-		//var time="current time is"+today.getHours()+" hours:"+ today.getMinutes()+" minutes";
+		
 		var time =today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 		speech.text=time;
-		//var t2=today.getHours()+":"+today.getMinutes()
 		var t2=time;
 		document.querySelector(".result").textContent=t2
-		/*
-			today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-		*/
 	}
 	if(msg.includes("download")||msg.includes("install")){
 		speech.text=`Opening in your browser, Please enable the popup settings in browser settings`;
